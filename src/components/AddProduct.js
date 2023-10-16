5237f04b358bb6dae8230a146caf175718668f35
gaouimounir <mounir.gaoui@outlook.com>
2023-10-16 16:58:17 +0200

    ajout d'element dans le formulaire

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -9,67 +9,142 @@ const AddProduct = () => {
   const [newProduct, setNewProduct] = useState({
     title: "",
     description: "",
-    price: 0,
+    categories: "",
+    basePrice: "",
+    salePrice: "",
     imageUrl: "",
   });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addProduct(newProduct));
-    setNewProduct({
-      title: "",
-      description: "",
-      price: 0,
-      imageUrl: "",
-    });
+    if (validateForm()) {
+      dispatch(addProduct(newProduct));
+      // Réinitialisez le formulaire après avoir ajouté le produit
+      setNewProduct({
+        title: "",
+        description: "",
+        categories: "",
+        basePrice: "",
+        salePrice: "",
+        imageUrl: "",
+      });
+    }
+  };
+
+  const validateForm = () => {
+    const errors = {};
+    let isValid = true;
+
+    if (!newProduct.title) {
+      errors.title = "Le titre est requis";
+      isValid = false;
+    }
+
+    if (!newProduct.description) {
+      errors.description = "La description est requise";
+      isValid = false;
+    }
+
+    if (!newProduct.categories) {
+      errors.categories = "Les catégories sont requises";
+      isValid = false;
+    }
+
+    if (isNaN(newProduct.basePrice) || newProduct.basePrice <= 0) {
+      errors.basePrice = "Le prix de base doit être un nombre positif";
+      isValid = false;
+    }
+
+    if (isNaN(newProduct.salePrice) || newProduct.salePrice <= 0) {
+      errors.salePrice = "Le prix soldé doit être un nombre positif";
+      isValid = false;
+    }
+
+    if (!newProduct.imageUrl) {
+      errors.imageUrl = "L'URL de l'image est requise";
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
   };
 
   return (
-    <div className="add-product-container">
-      <h2>Ajouter un nouveau produit :</h2>
-      <form onSubmit={handleSubmit} className="product-form">
-        <div className="form-group">
-          <label>Titre :</label>
+    <div>
+      <h2>Ajouter un nouveau produit</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Titre :</label>
           <input
             type="text"
+            id="title"
+            name="title"
             value={newProduct.title}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, title: e.target.value })
-            }
+            onChange={handleChange}
           />
+          {errors.title && <p>{errors.title}</p>}
         </div>
-        <div className="form-group">
-          <label>Description :</label>
+        <div>
+          <label htmlFor="description">Description :</label>
           <textarea
+            id="description"
+            name="description"
             value={newProduct.description}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, description: e.target.value })
-            }
+            onChange={handleChange}
           />
+          {errors.description && <p>{errors.description}</p>}
         </div>
-        <div className="form-group">
-          <label>Prix :</label>
+        <div>
+          <label htmlFor="categories">Catégories :</label>
+          <input
+            type="text"
+            id="categories"
+            name="categories"
+            value={newProduct.categories}
+            onChange={handleChange}
+          />
+          {errors.categories && <p>{errors.categories}</p>}
+        </div>
+        <div>
+          <label htmlFor="basePrice">Prix de base :</label>
           <input
             type="number"
-            value={newProduct.price}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, price: e.target.value })
-            }
+            id="basePrice"
+            name="basePrice"
+            value={newProduct.basePrice}
+            onChange={handleChange}
           />
+          {errors.basePrice && <p>{errors.basePrice}</p>}
         </div>
-        <div className="form-group">
-          <label>URL de l'image :</label>
+        <div>
+          <label htmlFor="salePrice">Prix soldé :</label>
           <input
-            type="text"
-            value={newProduct.imageUrl}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, imageUrl: e.target.value })
-            }
+            type="number"
+            id="salePrice"
+            name="salePrice"
+            value={newProduct.salePrice}
+            onChange={handleChange}
           />
+          {errors.salePrice && <p>{errors.salePrice}</p>}
         </div>
-        <button type="submit" className="submit-button">
-          Ajouter
-        </button>
+        <div>
+          <label htmlFor="imageUrl">URL de l'image :</label>
+          <input
+            type="url"
+            id="imageUrl"
+            name="imageUrl"
+            value={newProduct.imageUrl}
+            onChange={handleChange}
+          />
+          {errors.imageUrl && <p>{errors.imageUrl}</p>}
+        </div>
+        <button type="submit">Ajouter le produit</button>
       </form>
     </div>
   );
