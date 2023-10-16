08bb12459785cc3152c19cd0fe84a5e8bb466a3a
gaouimounir <mounir.gaoui@outlook.com>
2023-10-16 11:38:47 +0200

    utlisation du components ProductListe pour afficher les produits

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getProducts } from "./actions/product.action";
+import React from "react";
+import GetProductListe from "./components/ProductListe";
 
 function App() {
-  const dispatch = useDispatch();
-  const products = useSelector((state) => state.productReducer);
-
-  useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
   return (
-    <div>
-      <h1>Liste des produits :</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <img src={product.imageUrl} alt={product.title} />
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>Prix : {product.salePrice} €</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <GetProductListe />;
+    </>
   );
 }
 export default App;
--- src/components/ProductListe.js
+++ src/components/ProductListe.js
@@ -1,7 +1,27 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getProducts } from "../actions/product.action";
 
-const RenderProduct = ({ product }) => {
-  const [articleData, setArticleData] = useState(false);
+const GetProductListe = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.productReducer);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Liste des produits :</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.imageUrl} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Prix : {product.salePrice} €</p>
+        </div>
+      ))}
+    </div>
+  );
 };
-export default RenderProduct;
+
+export default GetProductListe;
